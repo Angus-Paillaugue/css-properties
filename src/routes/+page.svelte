6e43f258d567a26efef90ad79e95c42d671c7721
mere-patrie <angus.paillaugue40@gmail.com>
2023-10-24 10:01:23 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,72 @@
+<script>
+    let noItems = "3";
+    let gap = "10";
+    let flexDir = "row";
+    let justContent = "space-between";
+    let alignItems = "center";
+</script>
+
+
+<div class="w-full h-full min-h-[calc(100vh-1rem)] my-2 max-w-screen-md mx-auto border rounded-lg border-neutral-200 p-4 flex flex-col">
+    <div class="grid grid-cols-3 gap-2">
+        <div class="flex flex-col">
+            <p>Nombre d'éléments</p>
+            <select bind:value={noItems} name="Nombre d'éléments">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
+        </div>
+        
+        <div class="flex flex-col">
+            <p>gap</p>
+            <select bind:value={gap} name="gap">
+                <option value="0">0px</option>
+                <option value="10">10px</option>
+                <option value="20">20px</option>
+                <option value="30">30px</option>
+                <option value="40">40px</option>
+            </select>
+        </div>
+        
+        <div class="flex flex-col">
+            <p>flex-direction</p>
+            <select bind:value={flexDir} name="flex-direction">
+                <option value="row">row</option>
+                <option value="column">column</option>
+                <option value="row-reverse">row-reverse</option>
+                <option value="column-reverse">column-reverse</option>
+            </select>
+        </div>
+        
+        <div class="flex flex-col">
+            <p>justify-content</p>
+            <select bind:value={justContent} name="justify-content">
+                <option value="flex-start">flex-start</option>
+                <option value="flex-end">flex-end</option>
+                <option value="center">center</option>
+                <option value="space-between">space-between</option>
+                <option value="space-around">space-around</option>
+            </select>
+        </div>
+        
+        <div class="flex flex-col">
+            <p>align-items</p>
+            <select bind:value={alignItems} name="align-items">
+                <option value="stretch">stretch</option>
+                <option value="flex-start">flex-start</option>
+                <option value="flex-end">flex-end</option>
+                <option value="center">center</option>
+                <option value="baseline">space-between</option>
+            </select>
+        </div>
+    </div>
+
+    <div class="w-full grow flex p-4 text-white" style="flex-direction: {flexDir}; gap: {gap}px; justify-content: {justContent}; align-items: {alignItems};">
+        {#each {length : Number(noItems)} as _, i}
+            <div class="w-14 h-14 bg-red-600 flex flex-col items-center justify-center rounded">{i+1}</div>
+        {/each}
+    </div>
+</div>
